a824df2d8ead336a1d0e6cf66a10eae90b4231d6
AnhelinaDiakova <Gelja4ka@gmail.com>
2020-12-27 22:16:14 +0200

    add component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
+import paintings from './paintings.json';
+
+export default function App() {
+  return (
+    <div>
+      <Section title="Топ недели">
+        <PaintingList items={paintings} />
+      </Section>
+      <Section title="Лучшее"/>
+    </div>)
+}
